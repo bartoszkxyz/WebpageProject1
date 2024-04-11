40e12e72f96b17d75ae65defc455ab46307d5d67
bartoszkxyz <bartoszkxyz@protonmail.com>
2024-04-11 15:28:19 +0200

    first commit

---
+++ dist/main.script.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./dev/js/main.js ***!
+  \************************/
+// import './components/mobileMenu'
+/******/ })()
+;
+//# sourceMappingURL=main.script.js.map
